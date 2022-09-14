e5c30bf0f241d24a699ba715c8262e924009ecb4
Babar Ayub <babarayub35@gmail.com>
2022-09-14 14:11:17 +0500

    feat: UI for Virtualized List

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import { VirtualizedList } from './components/virtualized-list/VirtualizedList';
+/** @jsxImportSource @emotion/react */
+import { Global, css } from "@emotion/react";
+import { VirtualizedList } from "./components/virtualized-list/VirtualizedList";
 
 function App() {
-  return (
-    <React.Fragment>
-      <VirtualizedList />
-    </React.Fragment>
-  );
+	return (
+		<div
+			css={{
+				backgroundColor: "#e7e7e7",
+				display: "flex",
+				height: "calc(100vh - 8rem)",
+				alignContent: "flex-start",
+				justifyContent: "center",
+				padding: "4rem",
+			}}
+		>
+			<Global
+				styles={css`
+					body {
+						margin: 0;
+						font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+							"Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+							"Helvetica Neue", sans-serif;
+						-webkit-font-smoothing: antialiased;
+						-moz-osx-font-smoothing: grayscale;
+					}
+				`}
+			/>
+			<div
+				css={{
+					width: 1000,
+					display: "flex",
+					backgroundColor: "white",
+				}}
+			>
+				<VirtualizedList />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/virtualized-list/VirtualizedList.tsx
+++ src/components/virtualized-list/VirtualizedList.tsx
@@ -1,19 +1,46 @@
+import { css } from "@emotion/react";
+
 /** @jsxImportSource @emotion/react */
 export const VirtualizedList = () => {
 	return (
-		<div>
+		<div css={{ display: "flex", flexDirection: "column", width: "100%",     padding: '0rem 4rem' }}>
 			<ActionBar />
 			<List />
 		</div>
 	);
 };
 
+const columnStyle = css({
+	display: 'flex',
+  flexGrow: 1,
+	width: "33%",
+  justifyContent: 'center'
+});
+
+
+
+const listColumnStyle = css({
+  height: '100%',
+  alignItems: 'center',
+  border: '1px solid grey'
+})
+
+
+const buttonStyle = css({
+  backgroundColor: 'white',
+  fontSize: 12,
+  padding: 14,
+  cursor: 'pointer',
+  fontWeight: 600,
+})
+
 export const ActionBar = () => {
 	return (
-		<div>
-			<div> Virtualized list</div>
-			<div>
-				<button> Add new item</button>
+		<div css={{ display: "flex", height: "7rem", alignItems: 'center' }}>
+			<div css={columnStyle}></div>
+			<div css={[columnStyle, {fontWeight: 600, fontSize: 18}]}> Virtualized list</div>
+			<div css={[columnStyle, {justifyContent: 'end'}]}>
+				<button css={buttonStyle}> Add new item</button>
 			</div>
 		</div>
 	);
@@ -21,17 +48,23 @@ export const ActionBar = () => {
 
 export const ListItem = () => {
 	return (
-		<div>
-			<div> Item1 </div>
-			<div> Item1 Description</div>
-			<div> Item1 Price</div>
+		<div css={{display: 'flex', flexDirection: 'row', height: '4rem', alignItems: 'center'}}>
+			<div css={[columnStyle, listColumnStyle]}> Item1 </div>
+			<div css={[columnStyle, listColumnStyle]}> Item1 Description</div>
+			<div css={[columnStyle, listColumnStyle]}> Item1 Price</div>
 		</div>
 	);
 };
+// aka hack..
+const listBorder =css({
+  border: '3px solid grey',
+  borderLeftWidth: 2,
+  borderRightWidth: 2,
+})
 
 export const List = () => {
 	return (
-		<div>
+		<div css={listBorder}>
 			<ListItem />
 			<ListItem />
 			<ListItem />
