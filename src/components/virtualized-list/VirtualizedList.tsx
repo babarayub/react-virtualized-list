fc88e50a34a312ef1940cc34cbab1c317ca80dbc
Babar Ayub <babarayub35@gmail.com>
2022-09-14 12:40:42 +0500

    adding intial template draft

---
+++ src/components/virtualized-list/VirtualizedList.tsx
@@ -0,0 +1,36 @@
+export const VirtualizedList = () => {
+	return (
+		<></>
+	);
+};
+
+export const ActionBar = () => {
+	return (
+		<div>
+			<div> Virtualized list</div>
+			<div>
+				<button> Add new item</button>
+			</div>
+		</div>
+	);
+};
+
+export const ListItem = () => {
+	return (
+		<div>
+			<div> Item1 </div>
+			<div> Item1 Description</div>
+			<div> Item1 Price</div>
+		</div>
+	);
+};
+
+export const List = () => {
+	return (
+		<div>
+			<ListItem />
+			<ListItem />
+			<ListItem />
+		</div>
+	);
+};
